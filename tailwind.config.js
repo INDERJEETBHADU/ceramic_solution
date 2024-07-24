87bb73540b1ee443f126abddb551a5e2ed00caa6
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-24 13:50:56 +0530

    footer add

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,10 +30,10 @@ module.exports = {
         red: "#FF0000",
         pink: "#FFEBEB",
         darkpink: "#B400011A",
-        ebony_clay:"#1B2228"
+        cod_gray:"#141414"
       },
       backgroundImage: {
-        'hero': "url('./assets/images/webp/bg-hero.webp')",
+        'hero': "url('./assets/images/webp/hero_bg_img.webp')",
       },
       screens: {
         'sm': '576px',

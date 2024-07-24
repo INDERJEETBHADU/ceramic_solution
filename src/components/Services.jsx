0be3c2fc9b4b3c163d657b49a9d2a452d04a9538
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-24 11:28:20 +0530

    service jsx added

---
+++ src/components/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className="container mx-auto">
+        {/* common */}
+        <p className = "max-w-[592px] pt-5 text-lg leading-7 text-black text-start font-normal font-Plus pb-14">Lörem ipsum koda astrobel: sutaveligen. Rodod bänera viliga. Pregigt primasofi dede facebooka: förutom tivaligt. Fejkade</p>
+    </div>
+  )
+}
+
+export default Services

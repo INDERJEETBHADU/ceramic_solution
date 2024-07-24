95550615aa21897ccb8d8b2e25182ed91929fc96
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-24 12:00:21 +0530

    footer jsx addes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="container mx-auto">
+        <div className="flex flex-wrap -mx-5">
+            <div className="w-1/4 px-5">
+            
+            </div>
+            <div className="w-5/12 px-5"></div>
+            <div className="w-1/3 px-5"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import CommonHeading from '../common/CommonHeading'
 
 const Services = () => {
   return (
     <div className="container mx-auto">
-        {/* common */}
-        <p className = "max-w-[592px] pt-5 text-lg leading-7 text-black text-start font-normal font-Plus pb-14">Lörem ipsum koda astrobel: sutaveligen. Rodod bänera viliga. Pregigt primasofi dede facebooka: förutom tivaligt. Fejkade</p>
+    <CommonHeading text="We provide services in these areas" spacing="mb-5" align="text-start" />
+        <p className = "max-w-[592px] text-lg leading-7 text-black text-start font-normal font-Plus pb-14">Lörem ipsum koda astrobel: sutaveligen. Rodod bänera viliga. Pregigt primasofi dede facebooka: förutom tivaligt. Fejkade</p>
+        <div className="flex flex-wrap -mx-5">
+            <div className="px-5 w-4/12"></div>
+        </div>
     </div>
   )
 }
